0498521fd09b00192c6f2f27aff14e033ef6f383
Abhinandkbabu <56472637+Abhinandkbabu@users.noreply.github.com>
2023-10-26 14:53:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// const toggleBtn = document.querySelector('.toggle-btn');
+// const toggleBtnicon = document.querySelector('.toggle-btn i');
+// const dropdownmenu = document.querySelector('.dropdown');
+// console.log(toggleBtn);
+// toggleBtn.onclick = function () {
+//     dropdownmenu.classList.toggle('open');
+//     console.log("asd");
+//     var isopen = dropdownmenu.classList.contains('open')
+//     toggleBtnicon.classList = isopen
+//     ? 'fa-solid fa-xmark'ml
+//     : 'fa-solid fa-bars'
+// }
+function drop(){
+   
+    var navbar=document.getElementById("dropdown");
+    navbar.classList.toggle('show');
+}
